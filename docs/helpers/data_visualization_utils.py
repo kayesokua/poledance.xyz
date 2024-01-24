2f62b7be9bbc7a074802692e50c3f60ffc05e602
Kaye <kaye.sokua@code.berlin>
2024-01-24 02:55:41 +0100

    minor updates, upload drafts for data vis and pose difficulty classifer

--- docs/helpers/data_visualization_utils.py
+++ docs/helpers/data_visualization_utils.py
@@ -1,7 +1,12 @@
+import os
+import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 from matplotlib.patches import FancyArrowPatch, Arc
+from .file_system_utils import parse_video_filename
+
+
 
 def visualize_pose_dimension_lengths(filepath, row, focus_heights):
     fig, axs = plt.subplots(1, 2, figsize=(10, 5))
@@ -110,3 +115,74 @@ def visualize_pose_coordinates(filepath, row, focus_landmarks):
     axs[1].axis('off')
 
     plt.show()
+    
+def visualize_video_frames_by_second(annotated_image_path_dir, data):
+    import os
+    import cv2
+    from matplotlib import pyplot as plt
+
+    plt.rcParams.update({'font.size': 16})
+    plt.rcParams['figure.constrained_layout.use'] = True
+    
+    total_seconds = data['secs'].max() + 1
+    image_files = sorted([f for f in os.listdir(annotated_image_path_dir) if f.endswith('.png')])
+    
+    nrows = (total_seconds - 1) // 10 + 1
+    ncols = min(total_seconds, 10)
+    figh = 8 * nrows
+    fig, axs = plt.subplots(nrows, ncols, figsize=(24, figh))
+    if nrows == 1:
+        axs = [axs]
+    
+    used_seconds = set()
+    plot_count = 0
+    for image_file in image_files:
+        _, second, _ = parse_video_filename(image_file) 
+
+        if second not in used_seconds:
+            used_seconds.add(second)
+            row_idx, col_idx = divmod(plot_count, 10)
+            img = cv2.imread(os.path.join(annotated_image_path_dir, image_file))
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            axs[row_idx][col_idx].imshow(img)
+            axs[row_idx][col_idx].set_title(f"Time: {second}")
+            axs[row_idx][col_idx].axis('off')
+            plot_count += 1
+    
+    for i in range(plot_count, nrows * ncols):
+        row_idx, col_idx = divmod(i, 10)
+        axs[row_idx][col_idx].axis('off')
+
+    plt.show()
+    
+
+def visualize_roc_trends(data):
+    keys = ['dist_rgt_shoulder_to_wrist', 'dist_lft_shoulder_to_wrist']
+    
+    grouped_data = {key: data.groupby('secs')[key].mean() for key in keys}
+    total_seconds = len(data['secs'].unique())
+    nrows = total_seconds // 10 + (total_seconds % 10 > 0)
+    
+    fig, axs = plt.subplots(nrows, 1, figsize=(24, 4 * nrows), squeeze=False, constrained_layout=True)
+    
+    for i in range(nrows):
+        start_sec = i * 10
+        end_sec = start_sec + 10
+
+        for key in keys:
+            subset = grouped_data[key][start_sec:end_sec]
+            axs[i, 0].plot(subset.index, subset, label=key.capitalize())
+
+            threshold = 0.4
+            peaks = subset[np.abs(subset) > threshold]
+            axs[i, 0].scatter(peaks.index, peaks, label=f'Peaks > 40% {key.capitalize()}')
+
+        axs[i, 0].grid(True)
+        axs[i, 0].set_xlabel('Seconds')
+        axs[i, 0].set_ylabel('Rate of Change')
+        axs[i, 0].set_yticks(np.arange(-1, 1.1, 0.2))
+        axs[i, 0].set_title(f'Rate of Change Analysis: Seconds {start_sec} to {end_sec}')
+        axs[i, 0].legend()
+
+    plt.show()
+
