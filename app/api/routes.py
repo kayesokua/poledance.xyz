adae14bc2e5ba4b814d2b5e06192c3abc5081be0
Kaye <kaye.sokua@code.berlin>
2024-01-27 14:39:54 +0100

    new directory paths for serving videos and reading extension model

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, send_from_directory, current_app
+from flask import Blueprint, send_from_directory, current_app, send_file, send_from_directory
 from flask_login import login_required, current_user
 import os
 import pytz
@@ -10,7 +10,5 @@ tz = pytz.timezone('UTC')
 @login_required
 def serve_uploaded_video(author_id,id):
     filename = f'{id}.mp4'
-    filepath = os.path.join(current_app.config['UPLOAD_FOLDER'], author_id)
-    
-    print(filepath,filename)
-    return send_from_directory(filepath, filename, as_attachment=True)
+    filepath = os.path.join(current_app.config['SERVE_UPLOAD_FOLDER'],author_id)
+    return send_from_directory(filepath,filename)
--- config.py
+++ config.py
@@ -15,13 +15,17 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
-    UPLOAD_FOLDER = 'data/uploads/'
-    FRAME_OUTPUT_FOLDER = 'data/processed/'
+    UPLOAD_FOLDER = 'app/data/uploads/'
+    FRAME_OUTPUT_FOLDER = 'app/data/processed/'
+    SERVE_UPLOAD_FOLDER = 'data/uploads/'
+    SERVE_PROCESSED_FOLDER = 'data/processed/'
+    MODEL_PATH = 'app/extensions/models/pose_landmarker.task'
 
 class TestingConfig(Config):
     DEBUG = True
     TESTING = True
     WTF_CSRF_ENABLED = False
     SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI_TEST')
-    UPLOAD_FOLDER = 'data/uploads/'
-    FRAME_OUTPUT_FOLDER = 'data/processed/'
+    UPLOAD_FOLDER = 'app/data/uploads/'
+    FRAME_OUTPUT_FOLDER = 'app/data/processed/'
+    MODEL_PATH = 'app/extensions/models/pose_landmarker.task'
