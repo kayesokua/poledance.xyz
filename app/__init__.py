b780572c1e5e2d46cf0f82bb3bcb113a3f41cc77
Kaye <kaye.sokua@code.berlin>
2024-01-27 13:39:23 +0100

    blueprints for api and diary, add flask moment module for posts, add logic for index route

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request, session, flash, abort
-from flask_login import LoginManager
+from flask_login import LoginManager, current_user
+from flask_moment import Moment
 
 from flask_bootstrap import Bootstrap
 from flask_wtf.csrf import CSRFProtect
@@ -11,17 +12,24 @@ def create_app():
 
     app = Flask(__name__)
     app.config.from_object('config.DevelopmentConfig')
-    csrf = CSRFProtect(app)
     db.init_app(app)
+    csrf = CSRFProtect(app)
     bootstrap = Bootstrap(app)
+    moment = Moment(app)
+
 
     with app.app_context():
-        from app.models import User
+        from app.models import User, VideoPost
         db.create_all()
         migrate.init_app(app, db, compare_type=True)
     
+    from app.api.routes import bp as api
     from app.accounts.routes import bp as accounts
+    from app.diary.routes import bp as diary
+    
+    app.register_blueprint(api)
     app.register_blueprint(accounts)
+    app.register_blueprint(diary)
     
     app.add_url_rule("/sign-in", endpoint='accounts.sign_in')
     app.add_url_rule("/sign-up", endpoint='accounts.sign_up')
@@ -36,6 +44,9 @@ def create_app():
     
     @app.route("/")
     def index():
+        if current_user.is_authenticated:
+            return redirect(url_for("diary.all_dance_entries"))
+
         return render_template("/index.html", title="Cover")
 
     return app
