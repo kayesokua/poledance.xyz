11d78c44167d91a566299e8aa827674b365b2eff
Kaye <kaye.sokua@code.berlin>
2024-01-30 02:24:48 +0100

    fix pose dictionary search feature

--- app/api/routes.py
+++ app/api/routes.py
@@ -20,25 +20,43 @@ def serve_uploaded_video(author_id,id):
 @login_required
 def serve_static_dictionary_tricks(category):
     if category == 'tricks':
-        title = "Pole Tricks"
         dict_read = pd.read_csv(os.path.join('app','static', 'dictionary', 'tricks', 'pose_data.csv'))
         dict_data = dict_read.to_dict(orient='records')
-        
     elif category == 'legs':
-        title = "Leg Positions"
         dict_read = pd.read_csv(os.path.join('app','static', 'dictionary', 'positions','legs', 'pose_data.csv'))
         dict_data = dict_read.to_dict(orient='records')
-        
     elif category == 'body':
-        title = "Body Positions"
         dict_read = pd.read_csv(os.path.join('app','static', 'dictionary', 'positions','body', 'pose_data.csv'))
         dict_data = dict_read.to_dict(orient='records')
-        
     elif category == 'grip':
-        title = "Grip Positions"
         dict_read = pd.read_csv(os.path.join('app','static', 'dictionary', 'positions','grip', 'pose_data.csv'))
         dict_data = dict_read.to_dict(orient='records')
     else:
         return redirect(url_for('diary.all_dance_entries'))
-        
     return dict_data
+
+@bp.route('/processed/<author_id>/<id>')
+@login_required
+def serve_video_cover_page(author_id,id):
+    if current_user.is_authenticated and current_user.id == author_id:
+        filename = '0000_00000000.png'
+        filepath = os.path.join(current_app.config['SERVE_PROCESSED_FOLDER'],author_id,id,'annotated')
+        return send_from_directory(filepath,filename)
+    
+
+@bp.route('/processed/<author_id>/<id>/timeline')
+@login_required
+def serve_timeline_image(author_id,id):
+    if current_user.is_authenticated and current_user.id == author_id:
+        filename = 'timeline.png'
+        filepath = os.path.join(current_app.config['SERVE_PROCESSED_FOLDER'],author_id,id,'annotated')
+        return send_from_directory(filepath,filename)
+               
+
+@bp.route('/pdata/<author_id>/<id>')
+@login_required
+def serve_pose_data(author_id,id):
+    if current_user.is_authenticated and current_user.id == author_id:
+        filename = 'pose_data.csv'
+        filepath = os.path.join(current_app.config['SERVE_PROCESSED_FOLDER'],author_id,id)
+        return send_from_directory(filepath,filename)
