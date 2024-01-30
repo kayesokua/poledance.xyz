76f2d0e3900ad91bfcafe5a0eef69f33896f1bff
Kaye <kaye.sokua@code.berlin>
2024-01-30 12:39:39 +0100

    config cleanup

--- config.py
+++ config.py
@@ -4,28 +4,35 @@ basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
 
 class Config():
-    SECRET_KEY = environ.get('SECRET_KEY_DEV')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    pass
-    
-class DevelopmentConfig(Config):
-    DEBUG = True
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI_DEV')
-    SQLALCHEMY_ECHO = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    WTF_CSRF_ENABLED = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
     UPLOAD_FOLDER = 'app/data/uploads/'
     FRAME_OUTPUT_FOLDER = 'app/data/processed/'
     SERVE_UPLOAD_FOLDER = 'data/uploads/'
     SERVE_PROCESSED_FOLDER = 'data/processed/'
     MODEL_PATH = 'app/extensions/models/pose_landmarker.task'
-
+    pass
+    
 class TestingConfig(Config):
+    SECRET_KEY = environ.get('SECRET_KEY_TEST')
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI_TEST')
+    DEBUG = False
+    TESTING = True
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    WTF_CSRF_ENABLED = False
+    
+class DevelopmentConfig(Config):
+    SECRET_KEY = environ.get('SECRET_KEY_DEV')
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI_DEV')
     DEBUG = True
     TESTING = True
-    WTF_CSRF_ENABLED = False
-    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI_TEST')
-    UPLOAD_FOLDER = 'app/data/uploads/'
-    FRAME_OUTPUT_FOLDER = 'app/data/processed/'
-    MODEL_PATH = 'app/extensions/models/pose_landmarker.task'
+    SQLALCHEMY_ECHO = True
+    WTF_CSRF_ENABLED = True
+    
+class ProductionConfig(Config):
+    SECRET_KEY = environ.get('SECRET_KEY_PROD')
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI_PROD')
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_ECHO = True
+    WTF_CSRF_ENABLED = True
